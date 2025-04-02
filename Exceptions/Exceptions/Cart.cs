7dd21989de9a67181d66165fac3d607cd8c9ab85
ITHUB\Student <>
2025-04-02 14:15:18 +0300

    init

---
+++ Exceptions/Exceptions/Cart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetShop
+{
+    internal class Cart
+    {
+        Dictionary<Product, int> products = new Dictionary<Product, int>();
+
+        public void AddProduct(Product product, int quantity)
+        {
+            if (products.ContainsKey(product) && products.TryGetValue(product, out quantity))
+            {
+                products.Add(product, quantity);
+            }
+            else
+            {
+                throw new InsufficientExecutionStackException();
+            }
+        }
+    }
+}
---
+++ Exceptions/Exceptions/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetShop
+{
+    internal class Product
+    {
+        public string Name { get; private set; }
+        public int Price { get; private set; }
+        public int Stock { get; private set; }
+
+        public Product(string name, int price, int stock)
+        {
+            Name = name;
+            if (price <= 0)
+            {
+                throw new ArgumentException(nameof(price), "Price can not be less than zero");
+            }
+            if (stock <= 0)
+            {
+                throw new ArgumentException(nameof(stock), "Stock can not be less than zero");
+            }
+            Price = price;
+            Stock = stock;
+        }
+    }
+}
